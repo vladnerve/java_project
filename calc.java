e34df7fdfd12979be1b19febdef8bdd86dd891f6
vladnerve <vladnevr@gmail.com>
2020-05-20 21:48:32 +0400

    create calc

--- calc.java
+++ calc.java
@@ -2,7 +2,21 @@ public class calc {
 
 	public static void main(String[] arg) {
 		System.out.println("calc...");
-	
+		double first = Integer.valueOf(arg[0]);
+		double second = Integer.valueOf(arg[1]);
+		
+		double sum = first + second;
+		System.out.println("sum "+ sum);
+		
+		double sub = first - second;
+		System.out.println("sub "+ sub);
+		
+		double mult = first * second;
+		System.out.println("mult "+ mult);
+		
+		double div = first / second;
+		System.out.println("div "+ div);
+		
 	}
 
 }
